a78495d8cb7da92540963175ed76b1795d0a2ed7
Fredrik Simonsson <simson@thesimson.net>
2018-12-10 06:47:15 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ extern crate microbit;
 
 use microbit::hal::prelude::*;
 use microbit::hal::serial::BAUD115200;
+use microbit::hal::serial;
+use microbit::hal;
+
+
 
 use cortex_m_rt::entry;
 
@@ -32,9 +36,25 @@ fn main() -> ! {
         /* Split GPIO pins */
         let mut gpio = p.GPIO.split();
 
-        /* Initialise serial port on the micro:bit */
-        let (mut tx, mut rx) = microbit::serial_port(gpio, p.UART0, BAUD115200);
+      /* Set row of LED matrix to permanent high */
+        let mut pin13 = gpio.pin13.into_push_pull_output();
+        pin13.set_high();
 
+        /* Set 2 columns to output to control LED states */
+        let mut led1 = gpio.pin4.into_push_pull_output();
+        let mut led2 = gpio.pin6.into_push_pull_output();
+
+        /* Initialise serial port on the micro:bit */
+
+        /* Configure RX and TX pins accordingly */
+        let tx = gpio.pin24.into_push_pull_output().downgrade();
+        let rx = gpio.pin25.into_floating_input().downgrade();
+
+        /* Set up serial port using the prepared pins */
+        let (mut tx, mut rx) = serial::Serial::uart0(p.UART0, tx, rx, BAUD115200).split();
+
+
+        let mut spi = hal::
         /* Print a nice hello message */
 
         let s = b"Commands 1 2 3 :\r\n";
@@ -48,7 +68,11 @@ fn main() -> ! {
                 let s=hex(c);
                 let _ = s.into_iter().map(|c| block!(tx.write(*c))).last();
                 if c == 0x33 {
+                    led1.set_high();
+                    led2.set_low();
                 }else{
+                    led2.set_high();
+                    led1.set_low();
                     let c = c & 0xDF;
                     let _ = block!(tx.write(c));
                 }
