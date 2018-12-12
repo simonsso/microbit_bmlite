d1c2ba0dc78a025b5e8bf5b945fcf3b16c1d63b8
Fredrik Simonsson <simson@thesimson.net>
2018-12-12 12:55:31 +0900

    Wip added SPI

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ extern crate microbit;
 use microbit::hal::prelude::*;
 use microbit::hal::serial::BAUD115200;
 use microbit::hal::serial;
+use microbit::hal::spi;
+use microbit::hal::hal::blocking::spi::Transfer;
 use microbit::hal;
 
 
@@ -37,8 +39,8 @@ fn main() -> ! {
         let mut gpio = p.GPIO.split();
 
       /* Set row of LED matrix to permanent high */
-        let mut pin13 = gpio.pin13.into_push_pull_output();
-        pin13.set_high();
+      //  let mut pin13 = gpio.pin13.into_push_pull_output();
+      //  pin13.set_high();
 
         /* Set 2 columns to output to control LED states */
         let mut led1 = gpio.pin4.into_push_pull_output();
@@ -53,23 +55,42 @@ fn main() -> ! {
         /* Set up serial port using the prepared pins */
         let (mut tx, mut rx) = serial::Serial::uart0(p.UART0, tx, rx, BAUD115200).split();
 
+        let mut spix = hal::spi::Spi::spi0(p.SPI0,
+                 gpio.pin13.into_floating_input().downgrade(),
+                 gpio.pin15.into_push_pull_output().downgrade(),
+                 gpio.pin14.into_floating_input().downgrade());
 
-        let mut spi = hal::
+        //let mut spi = hal::
         /* Print a nice hello message */
 
-        let s = b"Commands 1 2 3 :\r\n";
+        let s = b"Hello tell me what you doing Commands 1 2 3 :\r\n";
         let _ = s.into_iter().map(|c| block!(tx.write(*c))).last();
 
         let s = b"Hej Knackt:\r\n";
         /* Endless loop */
+        let mut buf:[u8;2] = [0x1c,0];
+        let mut rbuf:[u8;2] = [0,0];
         loop {
             /* Read and echo back */
             if let Ok(c) = block!(rx.read()) {
                 let s=hex(c);
                 let _ = s.into_iter().map(|c| block!(tx.write(*c))).last();
                 if c == 0x33 {
+                    // Transfer::transfer(&mut spix,&mut buf);
+                    let _ = hal::hal::spi::FullDuplex::send(&mut spix, buf[0]).unwrap();
+
+                    rbuf[0] = hal::hal::spi::FullDuplex::read(&mut spix).unwrap();
+                    let _ = hal::hal::spi::FullDuplex::send(&mut spix, buf[1]).unwrap();
+
+                    rbuf[1] = hal::hal::spi::FullDuplex::read(&mut spix).unwrap();
+
                     led1.set_high();
                     led2.set_low();
+                    for i in buf.iter(){
+                        let s=hex(*i);
+                        let _ = s.into_iter().map(|c| block!(tx.write(*c))).last();
+                    }
+                    
                 }else{
                     led2.set_high();
                     led1.set_low();
@@ -78,7 +99,8 @@ fn main() -> ! {
                 }
             }
         }
-    }
+    };
+
 
     loop {
         continue;
