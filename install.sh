0e246814231c2439f8426546f2ac8768e49588e5
Fredrik Simonsson <simson@thesimson.net>
2019-01-01 16:46:01 +0900

    Install script copies hex file to gh_pages

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ do
     sleep 2;
     export WAITING=''
 done
+install -d docs/release
 echo arm-none-eabi-objcopy target/thumbv6m-none-eabi/debug/microbit /media/$USER/MICROBIT/out.hex -O ihex
-arm-none-eabi-objcopy target/thumbv6m-none-eabi/debug/microbit /media/$USER/MICROBIT/out.hex -O ihex
+arm-none-eabi-objcopy target/thumbv6m-none-eabi/debug/microbit docs/release/microbitbmlite.hex  -O ihex
+cp docs/release/microbitbmlite.hex /media/$USER/MICROBIT/
